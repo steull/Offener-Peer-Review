f8bba1e79017d305c7a572461736eaba7262809a
steull <stephanuller.su@gmail.com>
2022-10-11 13:34:53 +0200

    Check, if key exists in template and add comment

--- src/test_code/test_files/write_in_suggestions_template.py
+++ src/test_code/test_files/write_in_suggestions_template.py
@@ -10,16 +10,33 @@ def enter_suggestion_template(file: json):
 # def write_in_suggestion_template(file: json):
 
 
-
-def new_entry(comment: str):
+def new_entry(key: str, comment: str):
     templ = enter_suggestion_template("comment_files/template_comment_on_fields.json")
-    new_suggestion_entry = {"who": None,
-                            "comment": comment,
-                            "timestamp": None,
-                            "value_suggestions": None,
-                            "accepted": None}
-    templ["reviews"][0].get("loop").append(new_suggestion_entry)
+    key_val = key
+    if key_val is templ["reviews"][0].get("key"):
+        new_suggestion_entry = {"who": None,
+                                "comment": comment,
+                                "timestamp": None,
+                                "value_suggestions": None,
+                                "accepted": None}
+        templ["reviews"][0].get("loop").append(new_suggestion_entry)
+    else:
+        new_key_and_suggestion_entry = {
+            "key": key_val,
+            "loop": [
+                {
+                    "who": None,
+                    "comment": comment,
+                    "timestamp": None,
+                    "value_suggestions": None,
+                    "accepted": None
+                }
+            ]
+        }
+        templ["reviews"].append(new_key_and_suggestion_entry)
+
+    print(templ)
     # return new_suggestion_entry
 
 
-new_entry("Add description")
+new_entry("name", "Add description")
