f6ab3fb0977a9070d3185bc440fef9c385473b97
steull <stephanuller.su@gmail.com>
2022-10-12 12:26:48 +0200

    Read file with all keys and comments and add to template

--- src/test_code/test_files/write_in_suggestions_template.py
+++ src/test_code/test_files/write_in_suggestions_template.py
@@ -7,54 +7,28 @@ def enter_suggestion_template(file: json):
     return json_data
 
 
-# def write_in_suggestion_template(file: json):
+def enter_comments(file: json):
+    with open(file) as json_comments_file:
+        json_comments = json.load(json_comments_file)
+    return json_comments
 
 
-def new_entry(key: str, comment: str):
+def new_entry():
     templ = enter_suggestion_template("comment_files/comments_in_template_added.json")
-    key_val = key
-    j = 0  # Iterate over nested dicts, to get the key name
-    # If dict to list all keys and corresponding comments is empty
-    if not templ["reviews"]:
-        new_key_and_suggestion_entry = {
-            "key": key_val,
-            "loop": [
-                {
-                    "who": None,
-                    "comment": comment,
-                    "timestamp": None,
-                    "value_suggestions": None,
-                    "accepted": None
-                }
-            ]
-        }
-        templ["reviews"].append(new_key_and_suggestion_entry)
-    # If dict contain keys including another dict with at least one comment
-    else:
-        # Check ALL keys first, if key already exists
-        # If yes, append new dict within key
-        # If not, append new dict for the new key
-        existing_keys = []
-        for i in templ["reviews"]:
-            existing_keys.append(templ["reviews"][j].get("key"))
-            if templ["reviews"][j].get("key") == key_val:
-                new_suggestion_entry = {"who": None,
-                                        "comment": comment,
-                                        "timestamp": None,
-                                        "value_suggestions": None,
-                                        "accepted": None}
-                templ["reviews"][j].get("loop").append(new_suggestion_entry)
-                j += 1
-            else:
-                j += 1
+    keys_and_val_file = enter_comments("comment_files/comments.json")
+    for a in keys_and_val_file:
+        key_val = a.get("key")
+        comment_rev = a.get("comment")
 
-        if key_val not in existing_keys:
+        j = 0  # Iterate over nested dicts, to get the key name
+    # If dict to list all keys and corresponding comments is empty
+        if not templ["reviews"]:
             new_key_and_suggestion_entry = {
                 "key": key_val,
                 "loop": [
                     {
                         "who": None,
-                        "comment": comment,
+                        "comment": comment_rev,
                         "timestamp": None,
                         "value_suggestions": None,
                         "accepted": None
@@ -62,6 +36,39 @@ def new_entry(key: str, comment: str):
                 ]
             }
             templ["reviews"].append(new_key_and_suggestion_entry)
+        # If dict contain keys including another dict with at least one comment
+        else:
+            # Check ALL keys first, if key already exists
+            # If yes, append new dict within key
+            # If not, append new dict for the new key
+            existing_keys = []
+            for i in templ["reviews"]:
+                existing_keys.append(templ["reviews"][j].get("key"))
+                if templ["reviews"][j].get("key") == key_val:
+                    new_suggestion_entry = {"who": None,
+                                            "comment": comment_rev,
+                                            "timestamp": None,
+                                            "value_suggestions": None,
+                                            "accepted": None}
+                    templ["reviews"][j].get("loop").append(new_suggestion_entry)
+                    j += 1
+                else:
+                    j += 1
+
+            if key_val not in existing_keys:
+                new_key_and_suggestion_entry = {
+                    "key": key_val,
+                    "loop": [
+                        {
+                            "who": None,
+                            "comment": comment_rev,
+                            "timestamp": None,
+                            "value_suggestions": None,
+                            "accepted": None
+                        }
+                    ]
+                }
+                templ["reviews"].append(new_key_and_suggestion_entry)
 
     write_in_template(templ)
 
@@ -71,4 +78,4 @@ def write_in_template(comments_for_template):
         json.dump(comments_for_template, jsonFile, indent=4, ensure_ascii=True)
 
 
-new_entry("newDate", "Contributor: Add the correct date")
+new_entry()
